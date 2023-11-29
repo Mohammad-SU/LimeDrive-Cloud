78e119057a921c35e8309261e57e95afcbc5e61a
NeonTech707 <mohammad.su3@outlook.com>
2023-11-29 21:07:46 +0000

    delete change

--- backend/app/Http/Controllers/DeleteController.php
+++ backend/app/Http/Controllers/DeleteController.php
@@ -19,45 +19,42 @@ class DeleteController extends Controller
             'itemIds.*.id' => 'required'
         ]);
 
-        try {
-            DB::beginTransaction();
+        $deletedFolderIds = [];
+        $deletedFileIds = [];
 
-            $deletedFolderIds = [];
-            $deletedFileIds = [];
+        foreach ($itemIds['itemIds'] as $itemId) {
+            $id = $itemId['id'];
+            $isFolderId = Str::startsWith($id, 'd_');
 
-            foreach ($itemIds['itemIds'] as $itemId) {
-                $id = $itemId['id'];
-                $isFolderId = Str::startsWith($id, 'd_');
+            if ($isFolderId) {
+                $dbId = Str::after($id, 'd_'); // Remove 'd_' prefix for backend processing
+                $this->deleteFolder($dbId, $deletedFolderIds, $deletedFileIds);
+                $deletedFolderIds[] = 'd_' . $dbId; // Add back 'd_' prefix for the response
+            } 
+            else {
+                $file = File::find($id);
 
-                if ($isFolderId) {
-                    $dbId = Str::after($id, 'd_'); // Remove 'd_' prefix for backend processing
-                    $this->deleteFolder($dbId, $deletedFolderIds, $deletedFileIds);
-                    $deletedFolderIds[] = 'd_' . $dbId; // Add back 'd_' prefix for the response
-                } 
-                else {
-                    $file = File::find($id);
-
-                    $cloud_path = Helpers::convertAppPath(auth()->id(), $file->app_path, $id);
-                    $rcloneCommand = env('RCLONE_EXE_PATH') . " delete " . env('RCLONE_REMOTE') . ":LimeDriveBucket/{$cloud_path} --b2-hard-delete";
-                    exec($rcloneCommand); // Use rclone because backblaze hides files instead of deleting them permanently and instantly
-                    if (Storage::exists($cloud_path)) {
-                        throw new \Exception('Failed to delete file from cloud storage.');
-                    }
-                    $file->delete();
-                    $deletedFileIds[] = $id;
+                $cloud_path = Helpers::convertAppPath(auth()->id(), $file->app_path, $id);
+                $rcloneCommand = env('RCLONE_EXE_PATH') . " delete " . env('RCLONE_REMOTE') . ":LimeDriveBucket/{$cloud_path} --b2-hard-delete";
+                exec($rcloneCommand); // Use rclone because backblaze hides files instead of deleting them permanently and instantly
+                if (Storage::exists($cloud_path)) {
+                    return response()->json([
+                        'message' => 'Failed to delete a file from cloud storage.',
+                        'deletedFolderIds' => $deletedFolderIds,
+                        'deletedFileIds' => $deletedFileIds,
+                    ]);
                 }
+                $file->delete();
+                $deletedFileIds[] = $id;
             }
-
-            return response()->json([
-                'message' => 'Item(s) deleted successfully',
-                'deletedFolderIds' => $deletedFolderIds,
-                'deletedFileIds' => $deletedFileIds,
-            ]);
-        } 
-        catch (\Exception $e) {
-            DB::rollBack();
-            return response()->json(['message' => $e->getMessage()], 500);
         }
+
+        return response()->json([
+            'message' => 'Item(s) deleted successfully',
+            'deletedFolderIds' => $deletedFolderIds,
+            'deletedFileIds' => $deletedFileIds,
+        ]);
+        
     }
 
     private function deleteFolder($folderId, &$deletedFolderIds, &$deletedFileIds)
@@ -70,10 +67,8 @@ class DeleteController extends Controller
         foreach ($subfiles as $subfile) {
             $cloud_path = Helpers::convertAppPath(auth()->id(), $subfile->app_path, $subfile->id);
             $rcloneCommand = env('RCLONE_EXE_PATH') . " delete " . env('RCLONE_REMOTE') . ":LimeDriveBucket/{$cloud_path} --b2-hard-delete";
-            exec($rcloneCommand);
-            if (Storage::exists($cloud_path)) {
-                throw new \Exception('Failed to delete file from cloud storage.');
-            }
+            exec($rcloneCommand); // Add error handling for this in the future with DB transaction (currently not able to reverse all b2 deletions if a single subfile has an error, unlike DB deletions)
+
             $subfile->delete();
             $deletedFileIds[] = $subfile->id;
         }
